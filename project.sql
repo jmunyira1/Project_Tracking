442c77db04a996b16650e2fc3ff8a7eae5f34306
jmunyira1 <munyira@munyira.co.ke>
2026-02-22 16:14:04 +0300

    Initial commit

--- project.sql
+++ project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.3deb1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 22, 2026 at 12:36 PM
--- Server version: 10.11.15-MariaDB-cll-lve
--- PHP Version: 8.4.17
+-- Generation Time: Feb 22, 2026 at 01:13 PM
+-- Server version: 11.8.5-MariaDB-4 from Debian
+-- PHP Version: 8.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `munyirac_project`
+-- Database: `project`
 --
 
 -- --------------------------------------------------------
